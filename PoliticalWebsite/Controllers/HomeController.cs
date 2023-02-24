21d2a806d91522573ef6408dcb914c6c864a6e87
Ashwani Mishra <developer6.afluex@gmail.com>
2023-02-24 13:05:02 +0530

    changes

---
+++ PoliticalWebsite/Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using PoliticalWebsite.Filter;
+using PoliticalWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PoliticalWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Login()
+        //{
+        //    Session.Abandon();
+        //    return View();
+        //}
+
+        //public ActionResult LoginAction(Home obj)
+        //{
+        //    string FormName = "";
+        //    string Controller = "";
+        //    try
+        //    {
+        //        Home Modal = new Home();
+        //        DataSet ds = obj.Login();
+        //        if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+        //        {
+        //            if (ds.Tables[0].Rows[0]["Msg"].ToString() == "1")
+        //            {
+        //                if (ds.Tables[0].Rows[0]["UserType"].ToString() == "Admin")
+        //                {
+        //                    Session["Pk_adminId"] = ds.Tables[0].Rows[0]["Pk_adminId"].ToString();
+        //                    Session["LoginId"] = ds.Tables[0].Rows[0]["LoginId"].ToString();
+        //                    Session["UsertypeName"] = ds.Tables[0].Rows[0]["UsertypeName"].ToString();
+        //                    Session["Name"] = ds.Tables[0].Rows[0]["Name"].ToString();
+        //                    FormName = "AdminDashboard";
+        //                    Controller = "Admin";
+        //                }
+        //                else
+        //                {
+        //                    TempData["Login"] = "Incorrect LoginId Or Password";
+        //                    FormName = "Login";
+        //                    Controller = "Home";
+        //                }
+        //            }
+        //            else
+        //            {
+        //                TempData["Login"] = "Incorrect LoginId Or Password";
+        //                FormName = "Login";
+        //                Controller = "Home";
+
+        //            }
+        //        }
+        //        else
+        //        {
+        //            TempData["Login"] = "Incorrect LoginId Or Password";
+        //            FormName = "Login";
+        //            Controller = "Home";
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        TempData["Login"] = ex.Message;
+        //        FormName = "Login";
+        //        Controller = "Home";
+        //    }
+        //    return RedirectToAction(FormName, Controller);
+        //}
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Contact")]
+        [OnAction(ButtonName = "btnSave")]
+        public ActionResult ContactUs(Home obj)
+        {
+            try
+            {
+                obj.AddedBy = "1";
+                DataSet ds = obj.SaveContactUs();
+                if (ds != null && ds.Tables[0].Rows.Count > 0)
+                {
+                    if (ds.Tables[0].Rows[0][0].ToString() == "1")
+                    {
+                        TempData["Contact"] = "Contact Details Saved Successfully !!";
+                    }
+                    else if (ds.Tables[0].Rows[0][0].ToString() == "0")
+                    {
+                        TempData["Contact"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                    }
+                }
+                else
+                {
+                    TempData["Contact"] = ds.Tables[0].Rows[0]["ErrorMessage"].ToString();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                TempData["Contact"] = ex.Message;
+            }
+            return RedirectToAction("Contact", "Home");
+        }
+
+        public ActionResult Events()
+        {
+            return View(); 
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult NewsPressRelease()
+        {
+            return View();
+        }
+    }
+}
---
+++ PoliticalWebsite/Models/Home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PoliticalWebsite.Models
+{
+    public class Home
+    {
+        public string AddedBy { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Message { get; set; }
+        public string ExampleId { get; set; }
+        public string LoginId { get; set; }
+        public string Password { get; set; }
+
+
+        public DataSet Login()
+        {
+            SqlParameter[] para ={new SqlParameter ("@LoginId",LoginId),
+                                new SqlParameter("@Password",Password)};
+            DataSet ds = Connection.ExecuteQuery("Login", para);
+            return ds;
+        }
+
+        public DataSet SaveContactUs()
+        {
+            SqlParameter[] para = { 
+                new SqlParameter("@Name",Name),
+                 new SqlParameter("@Email",Email),
+                  new SqlParameter("@Mobile",Mobile),
+                   new SqlParameter("@Message",Message),
+                    new SqlParameter("@ExampleId",ExampleId),
+                     new SqlParameter("@AddedBy",AddedBy)
+            };
+            DataSet ds = Connection.ExecuteQuery("SaveContactDetails", para);
+            return ds;
+        }
+    }
+}
